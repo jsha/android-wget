95e525f1c67f65dfb5d884b615e84cd5931aaacc
Jacob Hoffman-Andrews <jsha@waimanu.(none)>
2009-11-05 14:43:28 -0800

    Apply Eclipse format tool to Wget.java and WgetTask.java

--- apps/wget/src/net/crud/wget/Wget.java
+++ apps/wget/src/net/crud/wget/Wget.java
@@ -1,6 +1,5 @@
 package net.crud.wget;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,36 +21,35 @@ import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-@SuppressWarnings("unused")
 public class Wget extends Activity {
-    private static final String OPTIONS = "options";
-    private static final String URLS = "urls";
-    WgetTask mWgetTask;
-    
+	private static final String OPTIONS = "options";
+	private static final String URLS = "urls";
+	WgetTask mWgetTask;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		// The options and urls fields are saved across invocations of the application.
+
+		// The options and urls fields are saved across invocations of the
+		// application.
 		// They are only stored if the user hits 'Run wget'.
 		SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
-		String options = prefs.getString(OPTIONS, "-E -H  -N -k -p -w 1");
-		String urls = prefs.getString(URLS, "hoffman-andrews.com");
-	    
+		String options = prefs.getString(OPTIONS, "-E -H  -N -k -p");
+		String urls = prefs.getString(URLS, "google.com");
+
 		EditText url_field = (EditText) findViewById(R.id.UrlField);
 		url_field.setText(urls);
 		EditText options_field = (EditText) findViewById(R.id.OptionsField);
 		options_field.setText(options);
-		
+
 		final Button engage = (Button) findViewById(R.id.engage);
 		engage.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				runWget();
 			}
 		});
-		
 
 		final Button kill = (Button) findViewById(R.id.kill);
 		kill.setOnClickListener(new View.OnClickListener() {
@@ -62,30 +60,31 @@ public class Wget extends Activity {
 			}
 		});
 
-		// When the user hits 'enter', consider that the same as pressing the 'engage' button.
-		url_field.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				runWget();
-				return true;
-			}
-		});		
-		
+		// When the user hits 'enter', consider that the same as pressing the
+		// 'engage' button.
+		url_field
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView v, int actionId,
+							KeyEvent event) {
+						runWget();
+						return true;
+					}
+				});
+
 		Button help = (Button) findViewById(R.id.HelpButton);
-		final Context context = this.getApplicationContext();
+
 		help.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				startActivity(new Intent(
-                        Wget.this, WgetHelp.class));
+				startActivity(new Intent(Wget.this, WgetHelp.class));
 
 			}
 		});
-		
+
 		Button about = (Button) findViewById(R.id.AboutButton);
 		about.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				startActivity(new Intent(
-                        Wget.this, WgetAbout.class));
+				startActivity(new Intent(Wget.this, WgetAbout.class));
 			}
 		});
 	}
@@ -101,12 +100,10 @@ public class Wget extends Activity {
 		SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = prefs.edit();
 		editor.putString(OPTIONS, options);
-		editor.putString(URLS, url);		
-        editor.commit();
-        
+		editor.putString(URLS, url);
+		editor.commit();
+
 		String args = options + " " + url;
-		String[] cmd = { "/system/bin/sh", "-c", wget + " " + args };
-		Log.d("wget", "Executing '" + wget + " " + args + "'");
 
 		TextView tv = (TextView) findViewById(R.id.output);
 		ScrollView sv = (ScrollView) findViewById(R.id.scrollview);
@@ -114,13 +111,14 @@ public class Wget extends Activity {
 		try {
 			this.copyBinary("wget");
 		} catch (IOException e) {
-		  	tv.setText(e.toString());
+			tv.setText(e.toString());
 		}
-		
+
 		Button run = (Button) findViewById(R.id.engage);
 		Button kill = (Button) findViewById(R.id.kill);
-        
-		// This is an AsyncTask that executes the wget binary and copies stdout into 'tv'.
+
+		// This is an AsyncTask that executes the wget binary and copies stdout
+		// into 'tv'.
 		mWgetTask = new WgetTask(tv, sv, run, kill);
 		mWgetTask.execute(wget + " " + args);
 	}
@@ -142,7 +140,7 @@ public class Wget extends Activity {
 			is.close();
 			String[] cmd = { "/system/bin/chmod", "0755", outFileName };
 
-			Process proc = Runtime.getRuntime().exec(cmd);
+			Runtime.getRuntime().exec(cmd);
 		}
 	}
 
--- apps/wget/src/net/crud/wget/WgetTask.java
+++ apps/wget/src/net/crud/wget/WgetTask.java
@@ -18,12 +18,12 @@ import android.widget.TextView;
 
 public class WgetTask extends AsyncTask<String, String, Boolean> {
 	TextView mLogTarget;
-    ScrollView mScroller;
-    int mPid;
-    Method mCreateSubprocess;
-    Button mRunButton;
-    Button mKillButton;
-    
+	ScrollView mScroller;
+	int mPid;
+	Method mCreateSubprocess;
+	Button mRunButton;
+	Button mKillButton;
+
 	WgetTask(TextView tv, ScrollView sc, Button runButton, Button killButton) {
 		tv.setText("");
 		mLogTarget = tv;
@@ -42,38 +42,47 @@ public class WgetTask extends AsyncTask<String, String, Boolean> {
 			File dir = new File(working_dir);
 			dir.mkdir();
 
-			// Inspired by http://remotedroid.net/blog/2009/04/13/running-native-code-in-android/
+			// Inspired by
+			// http://remotedroid.net/blog/2009/04/13/running-native-code-in-android/
 			Class<?> execClass = Class.forName("android.os.Exec");
 			Method createSubprocess = execClass.getMethod("createSubprocess",
-			  String.class, String.class, String.class, int[].class);
+					String.class, String.class, String.class, int[].class);
 			mCreateSubprocess = createSubprocess;
+
+			Log.d("wget", "Executing '" + command + "'");
 
 			// Executes the command.
 			// NOTE: createSubprocess() is asynchronous.
-            // 'exec' is key here, otherwise killing this pid will only kill the shell, and wget will go background. 
+			// 'exec' is key here, otherwise killing this pid will only kill the
+			// shell, and wget will go background.
 			int[] pids = new int[1];
-			FileDescriptor fd = (FileDescriptor)createSubprocess.invoke(
-			   null, "/system/bin/sh", "-c", "cd " + working_dir + "; exec " + command[0], pids);
-	        mPid = pids[0];
-	        
-			// Reads stdout.
-			// NOTE: You can write to stdin of the command using new FileOutputStream(fd).
-			FileInputStream in = new FileInputStream(fd);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in), 8096);
+			FileDescriptor fd = (FileDescriptor) createSubprocess.invoke(null,
+					"/system/bin/sh", "-c", "cd " + working_dir + "; exec "
+							+ command[0], pids);
+			mPid = pids[0];
 
-			//Old-style, has obnoxious buffering behaviour, don't know why.
-            //Process proc = Runtime.getRuntime().exec(command[0], null, dir);
-			//DataInputStream in = new DataInputStream(proc.getInputStream());
-			//BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-			
+			// Reads stdout.
+			// NOTE: You can write to stdin of the command using new
+			// FileOutputStream(fd).
+			FileInputStream in = new FileInputStream(fd);
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(in), 8096);
+
+			// Old-style, has obnoxious buffering behaviour, don't know why.
+			// Process proc = Runtime.getRuntime().exec(command[0], null, dir);
+			// DataInputStream in = new DataInputStream(proc.getInputStream());
+			// BufferedReader reader = new BufferedReader(new
+			// InputStreamReader(proc.getInputStream()));
+
 			while ((one_line = reader.readLine()) != null) {
 				publishProgress(one_line + "\n");
 			}
 			mPid = -1;
 		} catch (IOException e1) {
-			// Hacky: When the input fd is closed, instead of returning null from the next readLine()
-			// call, it seems that IOException is thrown.  So we ignore it.
-			
+			// Hacky: When the input fd is closed, instead of returning null
+			// from the next readLine()
+			// call, it seems that IOException is thrown. So we ignore it.
+
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e.getMessage());
 		} catch (SecurityException e) {
@@ -92,57 +101,61 @@ public class WgetTask extends AsyncTask<String, String, Boolean> {
 
 	// Runs on UI thread
 	public boolean killWget() {
-	    Log.d("blah", "blah");
 		if (mPid != -1) {
-		    try {
-		    	mCreateSubprocess.invoke(null, "/system/bin/sh", "-c", "kill -2 " + mPid, null);
-				 
+			try {
+				mCreateSubprocess.invoke(null, "/system/bin/sh", "-c",
+						"kill -2 " + mPid, null);
+
 				publishProgress("\nProcess " + mPid + " killed by user\n");
 			} catch (IllegalArgumentException e) {
 			} catch (IllegalAccessException e) {
 			} catch (InvocationTargetException e) {
-				// If we catch an exception trying to run kill, don't worry about it much.
+				// If we catch an exception trying to run kill, don't worry
+				// about it much.
 				// Wget will die on its own, eventually (probably)
 				return false;
 			}
 			return true;
-	    } else {
-	        return true;
-	    }
+		} else {
+			return true;
+		}
 	}
-	
+
 	protected void onProgressUpdate(String... progress) {
 		this.mLogTarget.append(progress[0]);
 		final ScrollView sc = this.mScroller;
-		// Put this on the UI thread queue so the text view re-renders before we try to scroll.
-		// Otherwise we fire too soon and there is no additional space to scroll to!
-        sc.post(new Runnable() {
-            public void run() {
-            	sc.smoothScrollBy(0, 1000); // Arbitrary number greater than line height
-            }
-        }); 
-    }
+		// Put this on the UI thread queue so the text view re-renders before we
+		// try to scroll.
+		// Otherwise we fire too soon and there is no additional space to scroll
+		// to!
+		sc.post(new Runnable() {
+			public void run() {
+				sc.smoothScrollBy(0, 1000); // Arbitrary number greater than
+											// line height
+			}
+		});
+	}
 
 	protected void onPostExecute(Boolean result) {
- 		onProgressUpdate("\nfinished\n");
- 		showRunButton();
-    }
+		onProgressUpdate("\nfinished\n");
+		showRunButton();
+	}
 
-    protected void onCancelled() {
-    	showRunButton();
-    }
-    
-    protected void onPreExecute() {
-    	showKillButton();
-    }
-    
-    protected void showRunButton() {
-	    mKillButton.setVisibility(View.INVISIBLE);
-	    mRunButton.setVisibility(View.VISIBLE);
-    }
-    
-    protected void showKillButton() {
-	    mKillButton.setVisibility(View.VISIBLE);
-	    mRunButton.setVisibility(View.INVISIBLE);
-    }
+	protected void onCancelled() {
+		showRunButton();
+	}
+
+	protected void onPreExecute() {
+		showKillButton();
+	}
+
+	protected void showRunButton() {
+		mKillButton.setVisibility(View.INVISIBLE);
+		mRunButton.setVisibility(View.VISIBLE);
+	}
+
+	protected void showKillButton() {
+		mKillButton.setVisibility(View.VISIBLE);
+		mRunButton.setVisibility(View.INVISIBLE);
+	}
 }
